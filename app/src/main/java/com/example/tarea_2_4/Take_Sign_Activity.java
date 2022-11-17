e51781e17276942a966a1a4e7c42890cfff1fa9a
AvailableNameWill <carloswill06@gmail.com>
2022-11-16 23:01:41 -0600

    Initial commit

---
+++ app/src/main/java/com/example/tarea_2_4/Take_Sign_Activity.java
@@ -0,0 +1,14 @@
+package com.example.tarea_2_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Take_Sign_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_sign);
+    }
+}
---
+++ app/src/main/java/com/example/tarea_2_4/View_Sign_Activity.java
@@ -0,0 +1,14 @@
+package com.example.tarea_2_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_Sign_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_sign);
+    }
+}
